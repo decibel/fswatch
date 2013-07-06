3477de86c04c079644859bae399e2102374ccced
Steven Lu <stevenlu443@gmail.com>
2013-07-05 21:16:38 -0400

    Fix whitespace

--- fswatch.c
+++ fswatch.c
@@ -5,8 +5,8 @@
 #include <CoreServices/CoreServices.h> 
 
 /* fswatch.c
- * 
- * usage: ./fswatch /some/directory[:/some/otherdirectory:...]  
+ *
+ * usage: ./fswatch /some/directory[:/some/otherdirectory:...]
  *
  * compile me with something like: gcc fswatch.c -framework CoreServices -o fswatch
  *
@@ -17,68 +17,67 @@ extern char **environ;
 //the command to run
 int count_chars(const char* string, char ch)
 {
-    int count = 0;
-    for(; *string; count += (*string++ == ch));
-    return count;
+	int count = 0;
+	for(; *string; count += (*string++ == ch));
+	return count;
 }
 // write out some info when there's any change in watched files
-void callback( 
-    ConstFSEventStreamRef streamRef, 
-    void *clientCallBackInfo, 
-    size_t numEvents, 
-    void *eventPaths, 
-    const FSEventStreamEventFlags eventFlags[], 
-    const FSEventStreamEventId eventIds[]) 
-{ 
+void callback(
+	ConstFSEventStreamRef streamRef,
+	void *clientCallBackInfo,
+	size_t numEvents,
+	void *eventPaths,
+	const FSEventStreamEventFlags eventFlags[],
+	const FSEventStreamEventId eventIds[])
+{
   pid_t pid;
   int   status;
 
-  for (int i=0; i<numEvents; ++i) {
-	const char* path = ((char **)eventPaths)[i];
-	int extra = count_chars(path, ' ');
-	if (extra) { // produce escaped spaces in the paths
-		char * z = malloc(strlen(path)+1+extra);
-		int cur = 0, zcur = 0;
-		while (*path) {
-			if (*path == ' ')
-				z[zcur++] = '\\';
-			z[zcur++] = path[cur++];
+	for (int i=0; i<numEvents; ++i) {
+		const char* path = ((char **)eventPaths)[i];
+		int extra = count_chars(path, ' ');
+		if (extra) { // produce escaped spaces in the paths
+			char * z = malloc(strlen(path)+1+extra);
+			int cur = 0, zcur = 0;
+			while (*path) {
+				if (*path == ' ')
+					z[zcur++] = '\\';
+				z[zcur++] = path[cur++];
+			}
+			printf("%x %s, ", eventFlags[i], z);
+		} else {
+			printf("%x %s, ", eventFlags[i], path);
 		}
-		printf("%x %s, ", eventFlags[i], z);
-	} else {
-		printf("%x %s, ", eventFlags[i], path);
 	}
-  }
-  printf("\n");
-  fflush(stdout);
-} 
- 
+	printf("\n");
+	fflush(stdout);
+}
+
 //set up fsevents and callback
 int main(int argc, char **argv) {
+	if(argc != 2) {
+		fprintf(stderr, "You must specify a directory to watch\n");
+		exit(1);
+	}
 
-  if(argc != 2) {
-    fprintf(stderr, "You must specify a directory to watch\n");
-    exit(1);
-  }
+	CFStringRef mypath = CFStringCreateWithCString(NULL, argv[1], kCFStringEncodingUTF8);
+	CFArrayRef pathsToWatch = CFStringCreateArrayBySeparatingStrings (NULL, mypath, CFSTR(":"));
 
-  CFStringRef mypath = CFStringCreateWithCString(NULL, argv[1], kCFStringEncodingUTF8); 
-  CFArrayRef pathsToWatch = CFStringCreateArrayBySeparatingStrings (NULL, mypath, CFSTR(":"));
+	void *callbackInfo = NULL;
+	FSEventStreamRef stream;
+	CFAbsoluteTime latency = 1.0;
 
-  void *callbackInfo = NULL; 
-  FSEventStreamRef stream; 
-  CFAbsoluteTime latency = 1.0;
-
-  stream = FSEventStreamCreate(NULL,
-    &callback,
-    callbackInfo,
-    pathsToWatch,
-    kFSEventStreamEventIdSinceNow,
-    latency,
+	stream = FSEventStreamCreate(NULL,
+	&callback,
+	callbackInfo,
+	pathsToWatch,
+	kFSEventStreamEventIdSinceNow,
+	latency,
 	kFSEventStreamCreateFlagFileEvents
-  ); 
+	);
 
-  FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode); 
-  FSEventStreamStart(stream);
-  CFRunLoopRun();
+	FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode); 
+	FSEventStreamStart(stream);
+	CFRunLoopRun();
 
 }
