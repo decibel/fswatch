fc004d60f8fefe20d2e38d1dfbf9003467071c2b
Steven Lu <stevenlu443@gmail.com>
2014-09-02 10:06:08 -0400

    fixed a hugely egregious bug (since i never tested using fswatch on files with spaces!) as well as adding a whole new bit of functionality for reducing the volume of path strings passed around in applications where cwd is well defined

--- fswatch.c
+++ fswatch.c
@@ -6,21 +6,39 @@
 
 /* fswatch.c
  *
- * usage: ./fswatch /some/directory[:/some/otherdirectory:...]
+ * usage: ./fswatch /some/directory[:/some/otherdirectory:...] [-bn]
+ *
+ * bn flag indicates that the returned paths should be direct relative paths. 
+ * Obviously you should not use when declaring multiple paths separated by 
+ * colons, ambiguities will arise.
  *
  * compile me with something like: gcc fswatch.c -framework CoreServices -o fswatch
  *
  * adapted from the FSEvents api PDF
 */
 
-extern char **environ;
-//the command to run
-int count_chars(const char* string, char ch)
+static inline int count_chars(const char* string, char ch)
 {
 	int count = 0;
 	for(; *string; count += (*string++ == ch));
 	return count;
 }
+
+// todo: make into a general helper just like count_chars
+static inline int last_slash(const char* string) {
+	int i;
+	for(i=strlen(string)-1;i;--i) {
+		if (string[i] == '\\') {
+			return i;
+		}
+	}
+	// the termination condition of the for loop (and the following return) 
+	// should be unnecessary since all paths have at least one slash, but I am 
+	// not about to inject a potential DoS vuln because of that logic either.
+	printf("wtf\n");
+	exit(-1);
+}
+
 // write out some info when there's any change in watched files
 void callback(
 	ConstFSEventStreamRef streamRef,
@@ -39,8 +57,8 @@ void callback(
 		if (extra) { // produce escaped spaces in the paths
 			char * z = malloc(strlen(path)+1+extra);
 			int cur = 0, zcur = 0;
-			while (*path) {
-				if (*path == ' ')
+			while (path[cur]) {
+				if (path[cur] == ' ')
 					z[zcur++] = '\\';
 				z[zcur++] = path[cur++];
 			}
@@ -53,11 +71,51 @@ void callback(
 	fflush(stdout);
 }
 
+// making code wet eliminates conditional check inside callback
+void callback_basename(
+	ConstFSEventStreamRef streamRef,
+	void *clientCallBackInfo,
+	size_t numEvents,
+	void *eventPaths,
+	const FSEventStreamEventFlags eventFlags[],
+	const FSEventStreamEventId eventIds[])
+{
+	pid_t pid;
+	int   status;
+
+	for (int i=0; i<numEvents; ++i) {
+		char* path = ((char **)eventPaths)[i];
+		// grab just the basename part
+		char* bn = path+last_slash(path)+1;
+		int extra = count_chars(bn, ' ');
+		if (extra) { // produce escaped spaces in the paths
+			char * z = malloc(strlen(bn)+1+extra);
+			int cur = 0, zcur = 0;
+			while (bn[cur]) {
+				if (bn[cur] == ' ')
+					z[zcur++] = '\\';
+				z[zcur++] = bn[cur++];
+			}
+			printf("%x %s, ", eventFlags[i], z);
+		} else {
+			printf("%x %s, ", eventFlags[i], bn);
+		}
+	}
+	printf("\n");
+	fflush(stdout);
+}
+
 //set up fsevents and callback
 int main(int argc, char **argv) {
-	if(argc != 2) {
+	if(argc != 2 && argc != 3) {
 		fprintf(stderr, "You must specify a directory to watch\n");
 		exit(1);
+	}
+
+	int basename = 0;
+	if (strncmp(argv[2], "-bn", 3) == 0) {
+		fprintf(stderr, "You have specified basename mode\n");
+		basename = 1;
 	}
 
 	CFStringRef mypath = CFStringCreateWithCString(NULL, argv[1], kCFStringEncodingUTF8);
@@ -68,7 +126,7 @@ int main(int argc, char **argv) {
 	CFAbsoluteTime latency = 1.0;
 
 	stream = FSEventStreamCreate(NULL,
-		&callback,
+		basename ? &callback_basename : &callback,
 		callbackInfo,
 		pathsToWatch,
 		kFSEventStreamEventIdSinceNow,
